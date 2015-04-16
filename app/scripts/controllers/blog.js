6306d79e70de82c052fd8b416d4fe9f387d3ef85
Hu Min <minispring1013@gmail.com>
2015-04-16 14:49:18 +0800

    add blog form

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -20,4 +20,16 @@ angular.module('bloggerApp')
     	{id:"2", create_at:"2015-04-13", comments:[],tags:"html5",title:"第二篇文章", img:"../images/main/2.jpg", content:"第二篇文章第一篇文章第一篇文章第一篇文章"}
     ];
 
+    $scope.addBlog = function(){
+    	console.log("abc")
+    	console.log($scope.title+'.......');
+    	var blog={title: $scope.title, content:$scope.content, id:'1', create_at:'2015-4-16',comments:[],img:"../images/main/3.jpg"};
+
+    	$scope.blogs.push(blog);
+
+    	$scope.title='';
+    	$scope.content='';
+    	blog='';
+    }
+
   });
